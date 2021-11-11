86fdbaeae8dbfcea8fba522b360d055bfb3af672
yashwanthphanindra2109 <yashwanth.puvvula@omniworksindia.com>
2021-11-11 21:12:51 +0530

    “test2”

--- App.electron.tsx
+++ App.electron.tsx
@@ -1,20 +1,71 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Button, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { View, Text } from "components";
+import { Button } from "react-native-paper"
 import Apps from "./src/Apps.electron";
+import { Calendar, modeToNum } from 'react-native-big-calendar';
+import { startClock } from "react-native-reanimated";
+import { Title } from "react-native-paper";
+
+
+const events = [
+  {
+    title: 'Meeting',
+    start: new Date(2021, 10, 11, 10, 0),
+    end: new Date(2021, 10, 11, 13, 30),
+  },
+  {
+    title: 'Coffee break',
+    start: new Date(2021, 10, 11, 15, 45),
+    end: new Date(2021, 10, 11, 18, 30),
+  },
+]
+const next = () => {
+  console.log("go to next");
+}
+const previous = () => {
+  console.log("go to Previous");
+}
+
 export default function App() {
 
   return (
-    <View >
-      <View >
-        {/* <Text>Yashwanth</Text> */}
-        <Apps />
+    // <View style={styles.container} className="pt-2">
+    //   <View style={styles.create}>
+    //     <Text style={{ fontWeight: "bold" }}>Testing paper and Example</Text>
+    //     {/* <Apps /> */}
+    //   </View>
+
+
+    //   <StatusBar style="dark" />
+    // </View>
+    <View>
+      <View className="row">
+
+        <View className="col-6" style={{ justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+          <Button onPress={() => { previous() }}>Previous</Button>
+        </View>
+        <View className="col-6" style={{ alignItems: 'flex-end' }}>
+          <Button onPress={() => { next() }}>Next</Button>
+        </View>
+
+      </View>
+      <View className="row">
+        <View className="col-12">
+          <Text style={{ alignItems: 'flex-start', fontSize: 20 }} className="ps-3">November</Text>
+        </View>
       </View>
 
 
-      <StatusBar style="dark" />
+      <View className="">
+        <Calendar events={events} height={600} date={new Date(2021, 10, 11, 10, 0)} activeDate={new Date(2021, 10, 11, 11, 0)} swipeEnabled />
+      </View>
+
+
     </View>
+
+
   );
 }
 
@@ -32,4 +83,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  table: {
+    width: "100%"
+  }
 });
--- App.tsx
+++ App.tsx
@@ -1,14 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Calendar, } from 'react-native-big-calendar';
+const events = [
+  {
+    title: 'Meeting',
+    start: new Date(2021, 9, 11, 10, 0),
+    end: new Date(2021, 9, 11, 13, 30),
+  },
+  {
+    title: 'Coffee break',
+    start: new Date(2021, 10, 11, 15, 45),
+    end: new Date(2021, 10, 11, 18, 30),
+  },
+]
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.create}>
-        <Text style={{ fontWeight: 'bold' }}>Test Electron using expo dependency</Text>
-      </View>
-      <StatusBar style="dark" />
+    // <View style={styles.container}>
+    //   <View style={styles.create}>
+    //     <Text style={{ fontWeight: 'bold' }}>Test Electron using expo dependency</Text>
+    //   </View>
+    //   <StatusBar style="dark" />
+    // </View>
+    <View>
+      <Calendar events={events} height={600} mode={'month'} swipeEnabled date={new Date(2021, 8, 21, 10, 0)} />
     </View>
   );
 }
